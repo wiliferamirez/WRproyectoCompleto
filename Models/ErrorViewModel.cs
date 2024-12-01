41aec60be73b4f0f0a0dc499e61123eca02d35bc
William Ramirez <wframirez98@gmail.com>
2024-12-01 15:49:40 -0500

    create project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WRproyectoCompleto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
